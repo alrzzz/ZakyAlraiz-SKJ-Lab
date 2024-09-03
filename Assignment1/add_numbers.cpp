1f2b2e58df607e7e7bd8d13a75ac0710ceafecaf
alrzzz <153983892+alrzzz@users.noreply.github.com>
2024-09-03 12:16:54 +0700

    Assignment 1 version 1

---
+++ Assignment1/add_numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x = 3;
+    int y = 1;
+    int sum = 3+1;
+
+    cout <<sum;
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+Nama : Zaky Alraiz Kadarisman   
+NIM : 23/516033/PA/22047 
